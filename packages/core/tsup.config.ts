ad17228d8651620936a2ce748dbc6a820beb2285
Uğur Eren <contact@ugureren.net>
2024-06-12 15:55:25 +0300

    Setup tsup

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {defineConfig, Options} from 'tsup';
+
+const getConfig = (config: Options): Options[] => {
+  return [
+    {
+      ...config,
+      format: ['cjs', 'esm'],
+      platform: 'node',
+      dts: true,
+    },
+    {
+      ...config,
+      format: ['iife'],
+      platform: 'browser',
+    },
+  ];
+};
+
+export default defineConfig([
+  // Default entrypoint
+  ...getConfig({
+    entry: ['src/index.ts'],
+    outDir: 'dist',
+    sourcemap: true,
+    clean: false,
+    globalName: 'adapter.core',
+  }),
+]);
